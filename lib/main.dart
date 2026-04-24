ce496a2d386223f2913f0efa03d25acd65284d00
balasrirajesh <narendrapurapubalasrirajesh@gmail.com>
2026-04-24 08:47:17 +0530

    Unify: Restore Session, Classroom, and Administrative pages from legacy codebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:device_preview/device_preview.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'app.dart';
+import 'package:dotenv/dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
